175c10e902f626a0c541814b0f2524c025e449b9
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 08:58:48 +0100

    Les variables

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<?php
-$operation =  3 + 4;
-$operation1 = 5 * 20;
-$operation2 = 45 / 5;
-echo $operation. ' ' .$operation1. ' ' .$operation2;
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 8</title>
+  </head>
+  <body>    
+    <?php
+    $operation =  3 + 4;
+    $operation1 = 5 * 20;
+    $operation2 = 45 / 5;
+    echo $operation. ' ' .$operation1. ' ' .$operation2;
+    ?>
+  </body>
+</html>
